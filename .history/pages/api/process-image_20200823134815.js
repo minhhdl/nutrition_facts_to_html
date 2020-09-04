b9ba4aae3c1fc12460e44bda552af2c81f23bf5b
Minh Huynh <minhhuynh261096@gmail.com>
2020-09-04 16:37:28 +0700

    init

--- .history/pages/api/process-image_20200823134815.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import formidable from 'formidable';
-
-export default (req, res) => {
-  const form = new formidable();
-  form.parse(req, (...args) => res.json({ ...args }));
-  // res.statusCode = 200
-  // res.json({ name: 'John Doe' })
-};
